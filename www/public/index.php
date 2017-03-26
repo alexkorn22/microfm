552115e3d52cd9faa6b81e5f44089fe6dbd7b307
Aleksandr <a@test.com>
2017-03-26 17:21:47 +0300

    Перенаправление на фронт контроллер

--- www/public/index.php
+++ www/public/index.php
@@ -1,5 +1,16 @@
 <?php
+
 require '../vendor/libs/functions.php';
 require '../vendor/core/Router.php';
-$query = rtrim($_SERVER['QUERY_STRING]'],'/');
-debug($_SERVER);
+
+$query = rtrim($_SERVER['REQUEST_URI'],'/');
+$query = ltrim($query,'/');
+
+Router::add('posts/add',['controller'=>'Posts','action'=>'add']);
+Router::add('',['controller'=>'Main','action'=>'index']);
+
+if (Router::matchRoute($query)){
+    //debug(Router::getRoute());
+}else {
+    echo '404';
+}
