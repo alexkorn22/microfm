7d0fe0fc8623aed5055e6845a0fcd322e016db1e
Aleksandr <a@test.com>
2017-03-27 21:29:42 +0300

    Подключен bootstrap

--- www/app/views/layouts/default.php
+++ www/app/views/layouts/default.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Default</title>
+    <link href="/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="/css/main.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
