3c5a33d5fc1182cee1858ec7d180f6ebfae525ac
Aleksandr <a@test.com>
2017-03-27 22:22:03 +0300

    Начало подключение к БД

---
+++ www/vendor/core/Db.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace vendor\core;
+
+class Db{
+
+    private static $instance;
+
+    /**
+     * @var \PDO
+     */
+    protected $pdo;
+
+    protected function __construct(){
+        $config = require ROOT . '/config/db.php';
+        $opt = [
+            \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
+            \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+            \PDO::ATTR_EMULATE_PREPARES   => false,
+        ];
+        $this->pdo = new \PDO($config['dsn'],$config['user'],$config['pass'],$opt);
+    }
+
+    /**
+     * @return Db
+     */
+    public static function getInstance(){
+        if (empty(self::$instance)) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    public function execute($sql){
+        $stmt = $this->pdo->prepare($sql);
+        return $stmt->execute();
+    }
+
+    public function query($sql){
+
+        $stmt = $this->pdo->prepare($sql);
+        $res = $stmt->execute();
+        if ($res != false) {
+            return $stmt->fetchAll();
+        }else {
+            return [];
+        }
+
+    }
+}
